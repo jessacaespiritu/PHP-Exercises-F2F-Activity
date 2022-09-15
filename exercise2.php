5131a6e4e70821c9ead5cec0432e9b5bf63a8084
jessacaespiritu <92897600+jessacaespiritu@users.noreply.github.com>
2022-09-15 11:10:50 +0800

    Add files via upload

---
+++ exercise2.php
@@ -0,0 +1,22 @@
+
+<?php
+$x=10;
+$y=7;
+
+$result=$x + $y;
+echo "$x + $y = $result. \n";
+
+$result=$x - $y;
+echo "$x - $y = $result. \n";
+
+$result=$x * $y;
+echo "$x * $y = $result. \n";
+
+$result=$x / $y;
+echo "$x / $y = $result. \n";
+
+$result=$x % $y;
+echo "$x % $y = $result. \n";
+
+?>
+
---
+++ exercise3.php
@@ -0,0 +1,22 @@
+<?php
+$num = 8;
+echo "Value is now $num. \n";  
+ 
+$num += 2;
+echo "Add 2. Value is now $num. \n";  
+ 
+$num -= 4;
+echo "Subtract 4. Value is now $num. \n";  
+ 
+$num *= 5;
+echo "Multiply by 5. Value is now $num. \n";  
+ 
+$num /= 3;
+echo "Divide by 3. Value is now $num. \n";  
+ 
+$num++;
+echo "Increment value by one. Value is now $num.\n";  
+ 
+$num--;
+echo "Decrement value by one. Value is now $num.";
+?>
